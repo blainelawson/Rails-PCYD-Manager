3b24f69f7110dcfc63519e298662fc412559893d
blainelawson <blaine.lawson@gmail.com>
2019-07-10 12:48:44 -0400

    added routes for members, directors, issues, and committes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,18 @@ Rails.application.routes.draw do
     resources :issues, only: [:new, :show, :edit, :index]
   end
 
-  resources :members # I need to do this again to make the rest of the routes available, yes??????
+  resources :members, except: [:show, :index] # I need to do this again to make the rest of the routes available, yes??????
 
   resources :directors
+
   resources :issues
-  resources :committees
+
+  resources :committees, only: [:show, :index] do
+    resources :members, only: [:new, :show, :edit, :index]
+  end
+
+  resources :committees, except: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # http://localhost:3000/rails/info/routes
 end
