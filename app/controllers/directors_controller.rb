20df052fb52f5bde9b708e56d8cab7700e6d9529
blainelawson <blaine.lawson@gmail.com>
2019-07-15 11:02:01 -0400

    Built director#new and #edit and views

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,12 +1,14 @@
 class DirectorsController < ApplicationController
     def new
         @director = Director.new
+        @members = Member.all
     end
 
     def create
         @director = Director.create(director_params)
         @director.chair = Member.find_by(id: params[:director][:chair])
         @director.save
+        redirect_to directors_path
     end
 
     def index
@@ -15,6 +17,21 @@ class DirectorsController < ApplicationController
 
     def edit
         @director = Director.find_by(id: params[:id])
+        @members = Member.all
+    end
+
+    def update
+        @director = Director.find_by(id: params[:id])
+        @director.update(director_params)
+        @director.chair = Member.find_by(id: params[:director][:chair])
+        @director.save
+        redirect_to directors_path
+    end
+
+    private
+
+    def director_params
+        params.require(:director).permit(:title, :email, :phone)
     end
 
 end
